2cf14792e1873e6f7671a030d2112ef928621357
chanaiagwata <chanaiagwata82@gmil.com>
2022-10-19 17:58:35 +0300

    adds class 'flascard'

--- src/app/flashcard/flashcard.component.ts
+++ src/app/flashcard/flashcard.component.ts
@@ -11,7 +11,7 @@ export class FlashcardComponent implements OnInit {
     new Flashcard(1, 'directive', 'classes that can add new behavior or modify structure of elements' ),
     new Flashcard(2, 'class', 'blueprint of an object'),
     new Flashcard(3, 'constructor', 'special function that creates and initializes an object instance'),
-    new Flashcard(3, 'data binding', 'mechanism for coordinating parts of template with a component')
+    new Flashcard(4, 'data binding', 'mechanism for coordinating parts of template with a component')
 
   ]
 
